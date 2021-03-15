c340f537b02ca02152808907513dd5385e4abc59
Érick <erick-sx-@hotmail.com>
2021-03-15 19:22:59 -0300

    novo sistema de chat
    recriação do sistema de chat

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "NPC file", menuName = "NPC Files Archive", order = 0)]
+
+public class NPC : ScriptableObject {
+    public string name;
+    [TextArea(4, 20)]
+    public string[] dialogue;
+    [TextArea(4,20)]
+    public string[] playerDialogue;
+}
